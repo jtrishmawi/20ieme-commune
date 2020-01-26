def970510d83a4d7f2e56ead343a5e8ca7886aa9
Rishmawi Jean-Tarek <jean-tarek@yusofleet.com>
2020-01-26 02:33:41 +0100

    Updates

---
+++ precache-manifest.e282496fb30cb507b1bf8ec3f790333e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d214fb5e15b0cdcfa99f521a85bb341d",
+    "url": "/20ieme-commune/index.html"
+  },
+  {
+    "revision": "acba835e34a814b61701",
+    "url": "/20ieme-commune/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7a886a2b728459a4679e",
+    "url": "/20ieme-commune/static/js/2.a36e3858.chunk.js"
+  },
+  {
+    "revision": "acba835e34a814b61701",
+    "url": "/20ieme-commune/static/js/main.39358ef5.chunk.js"
+  },
+  {
+    "revision": "0b6f4254b732115e4936",
+    "url": "/20ieme-commune/static/js/runtime-main.d49f05c8.js"
+  }
+]);
