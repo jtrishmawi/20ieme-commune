6057df42d8df676f90e40ba86940a1765c318e35
Jean-Tarek Rishmawi <tarek@Tarek.local>
2020-02-16 01:41:32 +0100

    Updates

---
+++ precache-manifest.3d0137bbebb51e73ef4d599a9960b1ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae144a23e06236f8099e8f20b56b6110",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c0f5af7673e041cc108",
+    "url": "/static/css/main.efdf188e.chunk.css"
+  },
+  {
+    "revision": "0bc5aa9e9babeb7aa32b",
+    "url": "/static/js/2.78903f5a.chunk.js"
+  },
+  {
+    "revision": "3c0f5af7673e041cc108",
+    "url": "/static/js/main.313488b3.chunk.js"
+  },
+  {
+    "revision": "07cd46d35ea16b881541",
+    "url": "/static/js/runtime-main.cf4f6ff8.js"
+  },
+  {
+    "revision": "bd9805cba4fdbd10e6f9d097dab9225f",
+    "url": "/static/media/Chalkduster.bd9805cb.ttf"
+  }
+]);
